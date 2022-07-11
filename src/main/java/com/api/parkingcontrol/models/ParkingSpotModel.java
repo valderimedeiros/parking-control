71d79a78664bf372c48c346166fca0c30f13b77d
valderi <valderimedeiros@gmail.com>
2022-07-10 23:06:09 -0300

    Implementation of a simple CRUD for a parking spot system.

--- src/main/java/com/api/parkingcontrol/models/ParkingSpotModel.java
+++ src/main/java/com/api/parkingcontrol/models/ParkingSpotModel.java
@@ -1,21 +1,42 @@
 package com.api.parkingcontrol.models;
 
-import lombok.RequiredArgsConstructor;
+import lombok.Data;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
+@Data
 @Entity
 @Table(name = "TB_PARKING_SPOT")
-@RequiredArgsConstructor
 public class ParkingSpotModel implements Serializable {
 
-    private static final long serialVersionUID = 1;
+    private static final long serialVersionUID = 1L;
 
     @Id
+    @Type(type="org.hibernate.type.UUIDCharType")
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
+    private UUID id;
+    @Column(nullable = false, unique = true , length = 10)
+    private String parkingSpotNumber;
+    @Column(nullable = false, unique = true, length = 7)
+    private String licensePlateCar;
+    @Column(nullable = false, length = 70)
+    private String brandCar;
+    @Column(nullable = false, length = 70)
+    private String modelCar;
+    @Column(nullable = false, length = 70)
+    private String colorCar;
+    @Column(nullable = false)
+    private LocalDateTime registrationDate;
+    @Column(nullable = false, length = 130)
+    private String responsibleName;
+    @Column(nullable = false, length = 30)
+    private String apartment;
+    @Column(nullable = false, length = 30)
+    private String block;
 
 
 }
